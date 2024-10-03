1d11ee4d3e66f419826b7d5d550e603733d3386b
Egor Vlasov <vlasoveb@gmail.com>
2024-10-03 21:45:40 +0300

    8_upd upd

--- module 4/4_task_8.cpp
+++ module 4/4_task_8.cpp
@@ -5,7 +5,7 @@ int main (){
     int salary2;
     int salary3;
     int maxSalary;
-    int mintSalary;
+    int minSalary;
     std::cout << "input employee's salary 1: ";
     std::cin >> salary1;
     std::cout << "input employee's salary 2: ";
@@ -15,18 +15,18 @@ int main (){
     std::cout << "=============processing=============\n";
     if (salary1 > 0 && salary2 > 0 && salary3 > 0){
         maxSalary = salary3;
-        mintSalary = salary1;
+        minSalary = salary1;
         if (salary1 > salary2) {
             maxSalary = salary1;
-            mintSalary = salary2;
+            minSalary = salary2;
             if (salary1 < salary3) maxSalary = salary3;
-            if (salary2 > salary3) mintSalary = salary3;
+            if (salary2 > salary3) minSalary = salary3;
         } else {
             if (salary2 > salary3) maxSalary = salary2;
         }
         std::cout << "hightest salary: " << maxSalary << " RUB\n";
-        std::cout << "lowest salary: " << mintSalary << " RUB\n";
-        std::cout << "the difference between the highest salary and the lowest: " << maxSalary - mintSalary << " RUB\n";
+        std::cout << "lowest salary: " << minSalary << " RUB\n";
+        std::cout << "the difference between the highest salary and the lowest: " << maxSalary - minSalary << " RUB\n";
         std::cout << "arithmetic average salary: " << (salary1 + salary2 + salary3)/3 << " RUB\n";  
     } else {
         std::cout << "Workers of the world UNITE! You should be paid money for your work!";
--- module 4/4_task_8_upd.cpp
+++ module 4/4_task_8_upd.cpp
@@ -14,13 +14,13 @@ int main (){
     std::cin >> salary3;
 
     std::cout << "=============processing=============\n";
-    
+
     if (salary1 > 0 && salary2 > 0 && salary3 > 0){
-        int hightestSalary = std::max({salary1, salary2, salary3});
-        int lowestSalary = std::min({salary1, salary2, salary3});
-        std::cout << "hightest salary: " << hightestSalary << " RUB\n";
-        std::cout << "lowest salary: " << lowestSalary << " RUB\n";
-        std::cout << "the difference between the highest salary and the lowest: " << hightestSalary - lowestSalary << " RUB\n";
+        int maxSalary = std::max({salary1, salary2, salary3});
+        int minSalary = std::min({salary1, salary2, salary3});
+        std::cout << "hightest salary: " << maxSalary << " RUB\n";
+        std::cout << "lowest salary: " << minSalary << " RUB\n";
+        std::cout << "the difference between the highest salary and the lowest: " << maxSalary - minSalary << " RUB\n";
         std::cout << "arithmetic average salary: " << (salary1 + salary2 + salary3)/3 << " RUB\n";  
     } else {
         std::cout << "Workers of the world UNITE! You should be paid money for your work!";
