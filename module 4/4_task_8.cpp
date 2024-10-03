93b9d6a613d93313d6cfe0cd7b4f117b3094bc6a
Egor Vlasov <vlasoveb@gmail.com>
2024-10-03 21:42:44 +0300

    8 upd

--- module 4/4_task_8.cpp
+++ module 4/4_task_8.cpp
@@ -4,29 +4,29 @@ int main (){
     int salary1;
     int salary2;
     int salary3;
-    int hightestSalary;
-    int lowestSalary;
-    std::cout << "input employee's salaty 1: ";
+    int maxSalary;
+    int mintSalary;
+    std::cout << "input employee's salary 1: ";
     std::cin >> salary1;
-    std::cout << "input employee's salaty 2: ";
+    std::cout << "input employee's salary 2: ";
     std::cin >> salary2;
-    std::cout << "input employee's salaty 3: ";
+    std::cout << "input employee's salary 3: ";
     std::cin >> salary3;
     std::cout << "=============processing=============\n";
     if (salary1 > 0 && salary2 > 0 && salary3 > 0){
-        hightestSalary = salary3;
-        lowestSalary = salary1;
+        maxSalary = salary3;
+        mintSalary = salary1;
         if (salary1 > salary2) {
-            hightestSalary = salary1;
-            lowestSalary = salary2;
-            if (salary1 < salary3) hightestSalary = salary3;
-            if (salary2 > salary3) lowestSalary = salary3;
+            maxSalary = salary1;
+            mintSalary = salary2;
+            if (salary1 < salary3) maxSalary = salary3;
+            if (salary2 > salary3) mintSalary = salary3;
         } else {
-            if (salary2 > salary3) hightestSalary = salary2;
+            if (salary2 > salary3) maxSalary = salary2;
         }
-        std::cout << "hightest salary: " << hightestSalary << " RUB\n";
-        std::cout << "lowest salary: " << lowestSalary << " RUB\n";
-        std::cout << "the difference between the highest salary and the lowest: " << hightestSalary - lowestSalary << " RUB\n";
+        std::cout << "hightest salary: " << maxSalary << " RUB\n";
+        std::cout << "lowest salary: " << mintSalary << " RUB\n";
+        std::cout << "the difference between the highest salary and the lowest: " << maxSalary - mintSalary << " RUB\n";
         std::cout << "arithmetic average salary: " << (salary1 + salary2 + salary3)/3 << " RUB\n";  
     } else {
         std::cout << "Workers of the world UNITE! You should be paid money for your work!";
