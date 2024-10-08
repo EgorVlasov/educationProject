aa2a2560cc8f741d59cd5e83d11cad6b8ffd7567
Egor Vlasov <vlasoveb@gmail.com>
2024-10-08 09:34:20 +0300

    task 5_1

---
+++ module_5/5_task_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL,".UTF8");
+    int altitude;
+    cout << "введите высоту полета: ";
+    cin >> altitude;
+
+    int speed;
+    cout << "введите скорость полета: ";
+    cin >> speed;
+
+    if ((speed >=750 && speed <= 850) && (altitude >=9000 && altitude <= 9500)) {
+        cout << "верный эшелон";
+    } else {
+        cout << "неверный эшелон";
+    }
+}
