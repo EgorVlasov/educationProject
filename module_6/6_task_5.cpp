1095003eeb6893af26a86895e29ee14e7498f888
Egor Vlasov <vlasoveb@gmail.com>
2024-10-26 13:06:31 +0300

    задание 8

--- module_6/6_task_5.cpp
+++ module_6/6_task_5.cpp
@@ -12,16 +12,23 @@ int main (){
     int counter = 0;
     int number = 1;
     int previousNumber = 0;
-
-    while (x > counter) {
-        int newNumber = number + previousNumber;
-        previousNumber = number;
-        if (counter < 1) previousNumber -= 1;
-        number = newNumber;
-        ++ counter;
-        cout << counter << ". " << number << "\n";
+    if (x > 0){
+        while (x > counter) {
+            int newNumber = number + previousNumber;
+            previousNumber = number;
+            if (counter < 1) previousNumber -= 1;
+            number = newNumber;
+            ++ counter;
+            cout << counter << ". " << number << "\n";
+            if (number >= 1836311903) break;
+        }
+        if (x == counter){
+            cout << "число Фибоначчи под порядковым номером " << x << " = " << number;
+        } else {
+            cout << "ошибка. слишком большое число";
+        }
+    } else {
+        cout << "число не может равняться 0 или быть отрицательным";
     }
-
-    cout << "число Фибоначчи под порядковым номером " << x << " = " << number;
     
 }
---
+++ module_6/6_task_6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main (){
+    setlocale(LC_ALL,".UTF8");
+
+    cout << "введите номер билета: ";
+    int x;
+    cin >> x;
+
+}
