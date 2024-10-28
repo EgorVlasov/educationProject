4a84b8843a07058e1153371d9570c7a18d09497e
Egor Vlasov <vlasoveb@gmail.com>
2024-10-28 16:30:04 +0300

    задание 6

--- module_6/6_task_6.cpp
+++ module_6/6_task_6.cpp
@@ -6,8 +6,36 @@ using namespace std;
 int main (){
     setlocale(LC_ALL,".UTF8");
 
-    cout << "введите номер билета: ";
-    int x;
-    cin >> x;
+    cout << "сумма вашего вклада: ";
+    int debit;
+    cin >> debit;
+    
+    cout << "процентная ставка: ";
+    int interestRate;
+    cin >> interestRate;
+
+    if (interestRate <= 100 && interestRate > 0){
+
+        cout << "желаемая сумма: ";
+        int desiredDebit;
+        cin >> desiredDebit;
+
+        if (desiredDebit > debit) {
+            int year;
+            while (debit < desiredDebit){
+            debit += debit*interestRate/100;
+            ++year;
+            cout << debit << "\n";
+            }
+            cout << "вам потребуется " << year << " года (лет)";
+
+        } else {
+            cout << "вам нет смысла вкладывать деньги, если желаемая сумма меньше или равна вашему вкладу";
+        }
+
+    } else {
+        cout << "процентная ставка не может быть выше 100%, так же она не может равняться 0 или быть отрицательной";
+    }
+
 
 }
