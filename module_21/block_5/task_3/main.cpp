370bd7f1dbc30fd5f5f6c6a6a026b86094a63d8f
Egor Vlasov <vlasoveb@gmail.com>
2025-11-10 18:31:20 +0300

    module_20_block_5_task_3

---
+++ module_21/block_5/task_3/main.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool has_input_error()
+{
+    if (cin.fail() || cin.peek() != '\n')
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cerr << "Input error. Try again" << endl;
+        return true;
+    }
+    else
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return false;
+    }
+}
+
+struct MathVector
+{
+
+public:
+    double x = 0;
+    double y = 0;
+    double z = 0;
+
+    MathVector(double newX = 0, double newY = 0, double newZ = 0) : x(newX), y(newY), z(newZ) {}
+
+    void input()
+    {
+        do
+        {
+            cout << "input vector coordinates (x, y, z): " << endl;
+            cin >> x >> y >> z;
+        } while (has_input_error());
+    }
+
+    MathVector add(const MathVector &other)
+    {
+        return MathVector(x + other.x, y + other.y, z + other.z);
+    }
+
+    MathVector subtract(const MathVector &other)
+    {
+        return MathVector(x - other.x, y - other.y, z - other.z);
+    }
+
+    MathVector scale(const int &factor)
+    {
+        return MathVector(x * factor, y * factor, z * factor);
+    }
+
+    double length()
+    {
+        return sqrt(x * x + y * y + z * z);
+    }
+
+    void normalize()
+    {
+        double mathVecLength = length();
+        if (mathVecLength < 1e-10)
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+            return;
+        }
+        x /= mathVecLength;
+        y /= mathVecLength;
+        z /= mathVecLength;
+    }
+
+    void print()
+    {
+        cout << "x: " << x << ", "
+             << "y: " << y << ", "
+             << "z: " << z << endl;
+    }
+};
+
+int main()
+{
+    MathVector vec_1;
+    MathVector vec_2;
+    MathVector vec_3;
+    int mathScalar = 0;
+    unsigned userCommands = 0;
+    while (true)
+    {
+        cout << "Input command:" << endl
+             << "1 - add vector" << endl
+             << "2 - scale vector" << endl
+             << "3 - subtract vector" << endl
+             << "4 - vector length" << endl
+             << "5 - normalize vector" << endl
+             << "6 - exit" << endl;
+        do
+        {
+            cin >> userCommands;
+        } while (has_input_error() || userCommands > 7);
+
+        switch (userCommands)
+        {
+        case 1:
+        {
+            cout << "Vector 1 " << endl;
+            vec_1.input();
+            cout << "Vector 2 " << endl;
+            vec_2.input();
+            vec_3 = vec_1.add(vec_2);
+            cout << "Result: " << endl
+                 << "Vector 3 " << endl;
+            vec_3.print();
+            break;
+        }
+        case 2:
+        {
+            cout << "Vector 1 " << endl;
+            vec_1.input();
+            do
+            {
+                cout << "Input mathScalar: " << endl;
+                cin >> mathScalar;
+            } while (has_input_error());
+            vec_3 = vec_1.scale(mathScalar);
+            cout << "Result: " << endl
+                 << "Vector 3 " << endl;
+            vec_3.print();
+            break;
+        }
+        case 3:
+        {
+            cout << "Vector 1 " << endl;
+            vec_1.input();
+            cout << "Vector 2 " << endl;
+            vec_2.input();
+            vec_3 = vec_1.subtract(vec_2);
+            cout << "Result: " << endl
+                 << "Vector 3 " << endl;
+            vec_3.print();
+            break;
+        }
+        case 4:
+        {
+            cout << "Vector 1 " << endl;
+            vec_1.input();
+            cout << "Vector 1 length: " << vec_1.length();
+            break;
+        }
+        case 5:
+        {
+            cout << "Vector 1 " << endl;
+            vec_1.input();
+            vec_1.normalize();
+            cout << "Vector 1 normalized: " << endl;
+            vec_1.print();
+            double vecLength = vec_1.length();
+            cout << "Checking, Vector 1 normalized length: " << vecLength << endl;
+            if (fabs(vecLength - 1.0) < 1e-10)
+            {
+                cout << "Correct!" << endl;
+            }
+            else
+            {
+                cout << "Error. Vector length != 1" << endl;
+            }
+            break;
+        }
+        case 6:
+            return 0;
+
+        default:
+            return 0;
+        }
+    }
+}
